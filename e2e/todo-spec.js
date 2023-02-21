e661fe828f8b5569629e7fce87b5d7c0db938252
tyshchuk-ak18 <tyshchuk_ak18@nuwm.edu.ua>
2023-02-21 18:25:40 +0200

    added some filles

---
+++ e2e/todo-spec.js
@@ -0,0 +1,9 @@
+describe('angularjs homepage todo list', function() {
+  it('should check title of page', function() {
+    browser.get('https://localhost:4200');
+    
+    browser.driver.getTitle().then(function(pageTitle) {
+      expect(pageTitle).toEqual('Example Domain');
+});
+});
+});
