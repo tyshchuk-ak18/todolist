6d64b02a2664965bc81ff47c5eff85dd823ed3fb
tyshchuk-ak18 <tyshchuk_ak18@nuwm.edu.ua>
2023-02-21 18:53:29 +0200

    roll back

--- e2e/conf.js
+++
@@ -1,4 +0,0 @@
-exports.config = {
-    seleniumAddress: 'http://localhost:4444',
-    specs: ['todo-spec.js']
-  };
--- e2e/todo-spec.js
+++
@@ -1,9 +0,0 @@
-describe('angularjs homepage todo list', function() {
-  it('should check title of page', function() {
-    browser.get('https://localhost:4200');
-    
-    browser.driver.getTitle().then(function(pageTitle) {
-      expect(pageTitle).toEqual('Example Domain');
-    });
-  });
-});
